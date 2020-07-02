ddb5cd419a44d15742782ba0754024e4ae420ee0
Ed Sweeney <ed@onextent.com>
2020-07-01 21:21:46 -0700

    observability basics

--- src/main/scala/somind/dtlab/routes/TypeApiRoute.scala
+++ src/main/scala/somind/dtlab/routes/TypeApiRoute.scala
@@ -1,15 +1,13 @@
 package somind.dtlab.routes
 
-import java.util.Date
-
 import spray.json._
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
 import akka.http.scaladsl.server.{Directives, Route}
 import com.typesafe.scalalogging.LazyLogging
 import somind.dtlab.HttpSupport
-import somind.dtlab.models.{JsonSupport, Name}
-import tech.navicore.lotsofnames.LotsOfPeople
+import somind.dtlab.models._
+import somind.dtlab.observe.Observer
 
 object TypeApiRoute
     extends JsonSupport
@@ -18,22 +16,31 @@ object TypeApiRoute
     with HttpSupport {
 
   def apply: Route = {
-    path(urlpath / Segment) { sourceId =>
+    path(urlpath / "type"/ Segment) { sourceId =>
       logRequest(s"$urlpath / $sourceId") {
         handleErrors {
           cors(corsSettings) {
             get {
-              val response = Name(java.util.UUID.randomUUID(),
-                                  new Date(),
-                                  sourceId,
-                                  LotsOfPeople(sourceId))
+              val response = DtType(List("one", "two", "three"))
+              Observer("type_route_get")
               complete(
                 HttpEntity(ContentTypes.`application/json`,
                            response.toJson.prettyPrint))
-            }
+            } ~ post {
+                decodeRequest {
+                  entity(as[DtType]) { dt =>
+                    logger.debug(s"creating new type: $dt")
+                    Observer("type_route_post")
+                    val response = dt
+                    complete(HttpEntity(ContentTypes.`application/json`,
+                      response.toJson.prettyPrint))
+                  }
+                }
+              }
           }
         }
       }
     }
   }
+
 }
